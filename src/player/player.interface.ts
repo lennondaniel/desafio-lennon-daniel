4c274c07b1e542cfe44cae21e401c1a6478d72ed
Lennon Daniel <lennon.v90@gmail.com>
2024-03-14 14:41:22 -0300

    move player interface

--- src/player/player.interface.ts
+++ src/player/player.interface.ts
@@ -92,3 +92,24 @@ export interface Player {
         };
     }]
 }
+
+export interface PlayerData extends Player {
+    player: {
+        id: number;
+        name: string;
+        firstname: string;
+        lastname: string;
+        age: number;
+        birth: {
+            date: string;
+            place: string;
+            country: string;
+        };
+        nationality: string;
+        height: string;
+        weight: string;
+        injured: boolean;
+        photo: string;
+        events?: number;
+    };
+}
