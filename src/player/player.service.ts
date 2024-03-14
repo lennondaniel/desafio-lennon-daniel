fd52fe68479adf3f917690def12d53214ea65143
Lennon Daniel <lennon.v90@gmail.com>
2024-03-14 11:21:00 -0300

    add import players

--- src/player/player.service.ts
+++ src/player/player.service.ts
@@ -3,10 +3,15 @@ import { InjectModel } from '@nestjs/mongoose';
 import { PlayerModel } from './player.schema';
 import { Model } from 'mongoose';
 import { Player } from './interfaces/player.interface';
+import { FootballApiService } from 'src/external-apis/football-api/football-api.service';
+import { FootballApiResponse } from 'src/external-apis/football-api/football-api.interface';
 
 @Injectable()
 export class PlayerService {
-    constructor(@InjectModel(PlayerModel.name) private readonly playerModel: Model<PlayerModel>){}
+    constructor(
+        @InjectModel(PlayerModel.name) private readonly playerModel: Model<PlayerModel>,
+        private readonly footballApiService: FootballApiService
+    ){}
 
     async createPlayer(player: Player): Promise<Player>{
         try {
@@ -15,6 +20,26 @@ export class PlayerService {
         } catch(error: any) {
             console.log(error);
             throw new Error(error);
+        }
+    }
+
+    async formatImportedPlayers(players: Player[]): Promise<void> {
+        for(const player of players) {
+            await this.createPlayer(player);
+        }
+    }
+
+    async importPlayersFromFootballApi(): Promise<void> {
+        let numberOfPlayers = 0;
+        let response: FootballApiResponse;
+        let paging = 1;
+        while(numberOfPlayers < 200) {
+            response = await this.footballApiService.getPlayers(paging);
+            if(response.paging.current < response.paging.total) {
+                this,this.formatImportedPlayers(response.response);   
+                numberOfPlayers += response.results;
+                paging++;
+            }
         }
     }
 }
