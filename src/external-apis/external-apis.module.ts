cd629ba7a084606f521f717cd726107061df2df2
Lennon Daniel <lennon.v90@gmail.com>
2024-03-14 11:19:50 -0300

    add export football api service

--- src/external-apis/external-apis.module.ts
+++ src/external-apis/external-apis.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { FootballApiService } from './football-api/football-api.service';
 
 @Module({
-  providers: [FootballApiService]
+  providers: [FootballApiService],
+  exports: [FootballApiService]
 })
 export class ExternalApisModule {}
