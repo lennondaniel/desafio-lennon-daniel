9a8d07794c2bdaeb592abf8d99536929a2acdba9
Lennon Daniel <lennon.v90@gmail.com>
2024-03-14 03:01:00 -0300

    add mongoose connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
-        })
+        }),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                uri: `mongodb://${configService.get<string>('MONGO_USERNAME')}:
+                ${configService.get<string>('MONGO_PASSWORD')}@
+                ${configService.get<string>('MONGO_HOST')}:
+                ${configService.get<string>('MONGO_PORT')}/
+                ${configService.get<string>('MONGO_DATABASE')}`.replace(/\s+/gm, '')
+            }),
+            inject: [ConfigService]
+        }),
     ],
     controllers: [AppController],
     providers: [AppService],
