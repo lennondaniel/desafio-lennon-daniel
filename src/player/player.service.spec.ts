25889986f48c77196fda5946bf1df2b5b832bbf5
Lennon Daniel <lennon.v90@gmail.com>
2024-03-14 03:48:51 -0300

    ad configuration to test service

--- src/player/player.service.spec.ts
+++ src/player/player.service.spec.ts
@@ -1,18 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PlayerService } from './player.service';
+import { getModelToken,  } from '@nestjs/mongoose';
+import { PlayerModel } from './player.schema';
+import { Model } from 'mongoose';
 
 describe('PlayerService', () => {
-  let service: PlayerService;
+    let service: PlayerService;
+    let model: Model<PlayerModel>;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [PlayerService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                PlayerService,
+                { provide: getModelToken(PlayerModel.name), useValue: model },
+            ],
+        }).compile();
 
-    service = module.get<PlayerService>(PlayerService);
-  });
+        service = module.get<PlayerService>(PlayerService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
